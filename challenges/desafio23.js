2cfd4e3a11273f4d79fb0d9f9b8e97191c23e34c
welitonlimaa <welitonlima159@gmail.com>
2023-04-17 18:23:41 -0300

    fix desafio 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,8 @@
+// use("commerce");
+db.produtos.updateMany(
+  {},
+  { $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } },
+  { upsert: true },
+);
+
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
