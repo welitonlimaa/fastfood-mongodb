3349fcc40f0700404f8e16af3251c6c7c1c20361
welitonlimaa <welitonlima159@gmail.com>
2023-04-17 16:37:42 -0300

    desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,10 @@
+// use("commerce");
+db.produtos.updateMany(
+  { nome: { $ne: "McChicken" } },
+  { $addToSet: { ingredientes: "ketchup" } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, ingredientes: 1, _id: 0 },
+);
