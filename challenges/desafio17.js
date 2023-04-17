3c8000325f796be8e15a905a3dc543ffe5115448
welitonlimaa <welitonlima159@gmail.com>
2023-04-17 17:31:01 -0300

    desafio 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,7 @@
+// use("commerce");
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: db.produtos.count(),
+});
+
+db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });
