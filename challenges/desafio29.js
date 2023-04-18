c6263fcd818e350d8c8e5ec2652d51f2144c7462
welitonlimaa <welitonlima159@gmail.com>
2023-04-17 21:25:32 -0300

    desafio 29

---
+++ challenges/desafio29.js
@@ -0,0 +1,11 @@
+// use("commerce");
+db.produtos.updateMany(
+  {},
+  {
+    $rename: {
+      descricao: "descricaoSite",
+    },
+  },
+);
+
+db.produtos.find({}, { nome: 1, descricaoSite: 1, _id: 0 });
